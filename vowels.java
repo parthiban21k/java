e1911e11c65077218d54f316a3d75f688e8f26df
parthiban21k <40587970+parthiban21k@users.noreply.github.com>
2018-06-26 10:16:02 +0530

    Update and rename vowel.java to vowels.java

---
+++ vowels.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class vowels
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String n=s.next();
+        switch(n)
+        {
+            case "a":
+                System.out.println("vowel");
+                break;
+                case "e":
+                System.out.println("vowel");
+                break;
+                case "i":
+                System.out.println("vowel");
+                break;
+                case "o":
+                    System.out.println("vowel");
+                    break;
+                    case "u":
+                        System.out.println("vowel");
+                        break;
+        }
+    }
+}
