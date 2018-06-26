f873d8832ed5f8b73f48bf9f6de15539966b7a12
parthiban21k <40587970+parthiban21k@users.noreply.github.com>
2018-06-26 09:59:50 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -5,7 +5,9 @@ class Oddeven
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        if(n%2==0){
+        if(n<0){
+            System.out.println("INVALID");
+        else if(n%2==0){
             System.out.println("EVEN");
         }
         else {
