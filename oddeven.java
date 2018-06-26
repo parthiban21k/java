8ca1aa941d81b41e03098cafe06c0187cad99b1b
parthiban21k <40587970+parthiban21k@users.noreply.github.com>
2018-06-26 09:55:00 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Oddeven
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n%2==0){
+            System.out.println("EVEN");
+        }
+        else {
+            System.out.println("ODD");
+        }
+        
+    }
+}
