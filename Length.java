c410e2b9ac2432aeeaf06c628fa1bbafd84f331d
parthiban21k <40587970+parthiban21k@users.noreply.github.com>
2018-06-27 10:42:00 +0530

    Create Length.java

---
+++ Length.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Length
+{
+    public static void main(String  args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int n=s.length();
+        System.out.println(n);
+    }
+}
