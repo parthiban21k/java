8f7cfc075503212ef1dd2212a0cbb08625bd60b5
parthiban21k <40587970+parthiban21k@users.noreply.github.com>
2018-08-22 11:41:50 +0530

    Update and rename Guvi2set8.java to Guvi2set1.java

---
+++ Guvi2set1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Guvi2set1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        double n3=0;
+        n3=Math.pow(n1,n2);
+        System.out.print((int)n3);
+    }
+}
