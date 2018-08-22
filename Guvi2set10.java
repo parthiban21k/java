ac20a6365d9c52a4fe3e669e8f4ead912fc1cc5f
parthiban21k <40587970+parthiban21k@users.noreply.github.com>
2018-08-22 11:57:14 +0530

    Update and rename Guvi2seta8.java to Guvi2set10.java

---
+++ Guvi2set10.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.math.*;
+class Guvi2set10
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=0;
+        for(int i=1;i<=5;i++)
+        {
+            n1=n*i;
+            System.out.print(n1+" ");
+        }
+    }
+}
