2ce1821c2a2de8e3d9be0ae12c7c643cde13337e
parthiban21k <40587970+parthiban21k@users.noreply.github.com>
2018-08-23 11:52:12 +0530

    Update and rename positive.java to Guvip3.java

---
+++ Guvip3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Guvip3
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String S=sc.nextLine();
+        String stl=".";
+        String s3=S+stl;
+        System.out.println(s3);
+    }
+}
+
