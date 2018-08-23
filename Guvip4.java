969a8dc492262318df4d03d30eff7439d5cf8e3a
parthiban21k <40587970+parthiban21k@users.noreply.github.com>
2018-08-23 11:53:28 +0530

    Update and rename Guvip3.java to Guvip4.java

--- Guvip4.java
+++ Guvip4.java
@@ -1,13 +1,14 @@
 import java.util.*;
-class Guvip3
+class Guvip4
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         String S=sc.nextLine();
+        StringBuffer sb=new StringBuffer(S);
         String stl=".";
-        String s3=S+stl;
-        System.out.println(s3);
+        sb.append(stl);
+        System.out.println(sb);
     }
 }
 
