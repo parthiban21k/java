dd3a8ab4fdea83aa653464b9d37212e38692ded9
parthiban21k <40587970+parthiban21k@users.noreply.github.com>
2018-08-23 12:11:15 +0530

    Update and rename Guvip9.java to Guvip2.java

---
+++ Guvip2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.math.*;
+class Guvip2
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=1;
+        for(int i=1;i<=n;i++)
+        {
+            n1=n1*i;
+        }
+        System.out.print(n1);
+    }
+}
+
